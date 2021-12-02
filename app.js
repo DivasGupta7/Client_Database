402e744032e72e4409d8457d6ca146e91d424693
DivasGupta7 <Dvsmathur7050@gmail.com>
2021-12-02 11:43:46 +0530

    updated app.js & index.html

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const mongoose = require('mongoose');
-const bodyparser = require('body-parser')
+// const bodyparser = require('body-parser')
 const port = 80;
 
 // for serving static files
@@ -19,11 +19,15 @@ app.set('views', path.join(__dirname,'views'))
 app.get('/', (req,res)=>{
   const params = {}
   res.status(200).render('index.html', params);
-})
-app.get("/team", (req, res)=>{ 
-   const params = {}
-   res.status(200).render('team.html', params);
 });
-app.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.get("/login_page.html", (req, res)=>{ 
+  const params = {}
+  res.status(200).render('login_page.html', params);
+});
+// app.get("/team", (req, res)=>{ 
+//    const params = {}
+//    res.status(200).render('team.html', params);
+// });
+app.listen(port, ()=>{
+  console.log(`The app started on port ${port}`);
 });
